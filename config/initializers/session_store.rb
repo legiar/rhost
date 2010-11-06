017c5bf5a026d7fdb2357855fe08ee2a0a7322c3
Mikhail Mikhaliov <legiar@gmail.com>
2010-11-06 13:57:42 +0200

    * setup session active_record store * add User scaffold * add UserSession model * add images and css

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rhost::Application.config.session_store :cookie_store, :key => '_rhost_session'
+#Rhost::Application.config.session_store :cookie_store, :key => '_rhost_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Rhost::Application.config.session_store :active_record_store
+Rhost::Application.config.session_store :active_record_store
