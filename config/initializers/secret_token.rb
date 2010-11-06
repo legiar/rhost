6ce88704ea08494ae0ee42e4de5e974c0ca0d4c0
Mikhail Mikhaliov <legiar@gmail.com>
2010-11-06 13:37:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhost::Application.config.secret_token = 'd466cbacaf3de09cb8269abd2d144b0122446d4069fb53ace633d59224432257fb54b5cb958eb9d4d117ced0f0c6c93ebb9a6d3649219127d5c1f3f5d77d257f'
