149f46c5ed987988345b2e8443e81f5dde6cb3d1
Mikhail Mikhaliov <legiar@gmail.com>
2010-12-03 07:55:14 +0200

    Rename Rhost application to RHost

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rhost::Application.config.secret_token = 'd466cbacaf3de09cb8269abd2d144b0122446d4069fb53ace633d59224432257fb54b5cb958eb9d4d117ced0f0c6c93ebb9a6d3649219127d5c1f3f5d77d257f'
+RHost::Application.config.secret_token = 'd466cbacaf3de09cb8269abd2d144b0122446d4069fb53ace633d59224432257fb54b5cb958eb9d4d117ced0f0c6c93ebb9a6d3649219127d5c1f3f5d77d257f'
